45b4a07ffd01796fb5fecf57bfa196408ea878ac
Himanshu Bhaisare <himanshu_bhaisare@hotmail.com>
2015-02-22 01:19:14 -0800

    Product model

---
+++ models/Product.php
@@ -0,0 +1,181 @@
+<?php
+
+class Product extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var string
+     */
+    protected $product_name;
+
+    /**
+     *
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     *
+     * @var string
+     */
+    protected $sku;
+
+    /**
+     *
+     * @var integer
+     */
+    protected $quantity;
+
+    /**
+     *
+     * @var integer
+     */
+    protected $price;
+
+    /**
+     * Method to set the value of field product_name
+     *
+     * @param string $product_name
+     * @return $this
+     */
+    public function setProductName($product_name)
+    {
+        $this->product_name = $product_name;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field id
+     *
+     * @param integer $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field sku
+     *
+     * @param string $sku
+     * @return $this
+     */
+    public function setSku($sku)
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field quantity
+     *
+     * @param integer $quantity
+     * @return $this
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field price
+     *
+     * @param integer $price
+     * @return $this
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Returns the value of field product_name
+     *
+     * @return string
+     */
+    public function getProductName()
+    {
+        return $this->product_name;
+    }
+
+    /**
+     * Returns the value of field id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Returns the value of field sku
+     *
+     * @return string
+     */
+    public function getSku()
+    {
+        return $this->sku;
+    }
+
+    /**
+     * Returns the value of field quantity
+     *
+     * @return integer
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Returns the value of field price
+     *
+     * @return integer
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @return Product[]
+     */
+    public static function find($parameters = array())
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * @return Product
+     */
+    public static function findFirst($parameters = array())
+    {
+        return parent::findFirst($parameters);
+    }
+
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'product_name' => 'product_name', 
+            'id' => 'id', 
+            'sku' => 'sku', 
+            'quantity' => 'quantity', 
+            'price' => 'price'
+        );
+    }
+
+}
